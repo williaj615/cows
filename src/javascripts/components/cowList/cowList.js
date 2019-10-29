59345604ca125f7142ddc61c1e731e2bf0f4583c
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-10-28 19:14:30 -0500

    printing cows to console with a promise

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -4,13 +4,7 @@ import cowData from '../../helpers/data/cows';
 const buildCows = () => {
     cowData.getCows()
     .then((response) => {
-        const demCows = response.data.cows;
-        const cows = [];
-        Object.keys(demCows).forEach((cowId) => {
-            demCows[cowId].id = cowId;
-            cows.push(demCows[cowId]);
-        });
-        console.log('it worked', cows);
+        console.log('cow array from cowList', response)
     }).catch((error) => {
         console.error('it broke', error);
     });
